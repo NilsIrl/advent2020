8a4fe7464f5ac715977e75170a646d9a35f5b5ab
Nils <nils@nilsand.re>
2020-12-02 12:56:12 +0000

    Add day2 part2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -8,17 +8,16 @@ fn main() {
             .lines()
             .filter(|line| {
                 let mut split = line.as_ref().unwrap().split(' ');
-                let (min, max) = {
+                let (min, max): (usize, usize) = {
                     let mut range = split.next().unwrap().split('-');
                     (
                         range.next().unwrap().parse().unwrap(),
                         range.next().unwrap().parse().unwrap(),
                     )
                 };
-                let c = split.next().unwrap().chars().next().unwrap();
-                let password = split.next().unwrap();
-                let count = password.matches(c).count();
-                count >= min && count <= max
+                let c = split.next().unwrap().chars().next().unwrap() as u8;
+                let password = split.next().unwrap().as_bytes();
+                (password[min - 1] == c) != (password[max - 1] == c)
             })
             .count()
     );
